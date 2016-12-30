d661c56050b2e0eed8f247da4f07703d57ae627d
Atila Romero <atilaromero@gmail.com>
2016-12-30 10:55:25 -0200

    log messages, trigger udev, only change to todo when in hold

--- imager.js
+++ imager.js
@@ -2,24 +2,31 @@
 
 const onDevice = require('./lib/onDevice')
 const runimager = require('./lib/runimager')
+const exec = require('./lib/exec')
 
 onDevice((device, docs) => {
   if (docs.length === 0) {
-    console.log('No material found with these properties in the database.')
+    console.log(device.device, '- No material found with these properties in the database.')
   } else if (docs.length === 1) {
     let doc = docs[0]
     runimager(device, doc)
     .then(() => {
-      doc.state = 'todo'
-      return doc.save()
+      if (doc.state === 'hold') {
+        doc.state = 'todo'
+        return doc.save()
+      } else {
+        return doc
+      }
     })
     .then((doc) => {
-      console.log('Ok', doc.material)
+      console.log(device.device, doc.material, 'Ok')
     })
   } else {
-    console.log('Multiples entries found matching current device:')
+    console.log(device.device, '- Multiples entries found matching current device:')
     docs.forEach(doc => {
       console.log(doc.material)
     })
   }
 })
+
+exec('udevadm trigger -s block')
