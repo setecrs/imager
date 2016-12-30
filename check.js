835aa6307ddd2ab921104f8e4b870329a637d88a
Atila Romero <atilaromero@gmail.com>
2016-12-30 11:16:01 -0200

    check

--- check.js
+++ check.js
@@ -2,16 +2,18 @@
 
 const onDevice = require('./lib/onDevice')
 const checkMat = require('./lib/checkMat')
+const exec = require('./lib/exec')
 
 onDevice((device, docs) => {
+  console.log('device', device.device)
   if (docs.length === 0) {
-    console.log('device', device.device)
     console.log('No material found with these properties in the database.')
   } else {
     docs.forEach(doc => {
-      console.log('device', device.device)
       console.log('image path', doc.path)
       checkMat(device, doc)
     })
   }
 })
+
+exec('udevadm trigger -s block')
